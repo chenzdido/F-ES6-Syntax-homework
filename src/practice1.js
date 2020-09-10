ad8bd2647f125ebf46612c3e3196564134764cbd
chenzdido <chenzdido@gmail.com>
2020-09-11 01:20:38 +0800

    [zhe] feat: complete practice1 & practice1.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData=(input) => {
+  var keys =[];
+  var output=[];
+  for(let key of input.column){
+    keys.push(key.name);
+  }
+  for(let value of input.data){
+    let ob={};
+    for(let i = 0 ; i < value.length; i++){
+      ob[keys[i]]=value[i]
+    }
+    output.push(ob);
+  }
+  return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject
+const inject = (items, sections) =>{
+  for(let i = 0 ; i < sections.length; i++){
+    items.splice(sections[i].index+i,0,sections[i].content)
+  }
+  return items;
+}
 export { inject };
