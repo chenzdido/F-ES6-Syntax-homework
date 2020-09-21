f354e5899dcb9797fdeea88edff390695d910411
kevindongzg <dongkevin94@gmail.com>
2020-09-21 21:18:13 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,11 +1,16 @@
+// TODO 使用解构的方式取出data与column
 const parseData=(input) => {
+  // TODO 没有必要声明keys
   var keys =[];
+  // TODO 使用map做数组与数组的转化，不用声明output
   var output=[];
+  // TODO 没有必要声明keys及以下内容
   for(let key of input.column){
     keys.push(key.name);
   }
   for(let value of input.data){
     let ob={};
+    // TODO 可以使用其他非for循环形式
     for(let i = 0 ; i < value.length; i++){
       ob[keys[i]]=value[i]
     }
--- src/practice2.js
+++ src/practice2.js
@@ -1,5 +1,6 @@
 const inject = (items, sections) =>{
   for(let i = 0 ; i < sections.length; i++){
+    // TODO 这样做需要保证sections顺序按照index排序才不会有错
     items.splice(sections[i].index+i,0,sections[i].content)
   }
   return items;
